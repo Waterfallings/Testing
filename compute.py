4e1f632482329f4e6ce057cd79653cec9f29eb62
Bam <bamidele.jay420@gmail.com>
2023-12-04 14:43:31 +0000

    Add a class for math functions

---
+++ compute.py
@@ -0,0 +1,13 @@
+class Compute:
+    def__init__(self, operator, operands):
+    self.operator = operator
+    self.operands = operands
+
+    def add(self):
+        pass
+    def subtract(self):
+        pass
+    def divide(self):
+        pass
+    def multiply(self):
+        pass                    
