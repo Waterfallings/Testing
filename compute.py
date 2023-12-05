5a550139b75652008e1f75e86483839c996d8938
Bam <bamidele.jay420@gmail.com>
2023-12-05 11:29:06 +0000

    Add support for exponents

--- compute.py
+++ compute.py
@@ -10,4 +10,9 @@ class Compute:
     def divide(self):
         pass
     def multiply(self):
-        pass                    
+        pass
+
+    def exponent(self):
+        num_exponent = self.operands[0] ** self.operands[1]
+        print(num_exponent)
+        
